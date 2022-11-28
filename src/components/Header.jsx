bc025dd68171829c19c1dcebcab8d01e147e2aa7
Yusufaky <marcosta71999@gmail.com>
2022-11-28 18:38:13 +0000

    14

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+function Header({ text, bgColor, textColor }) {
+  const headerStyle = { backgroundColor: bgColor, color: textColor };
+  return (
+    <header style={headerStyle}>
+      <div className="container">
+        <h1>{text}</h1>
+      </div>
+    </header>
+  );
+}
+Header.defaultProps = {
+  text: "Hello World Props",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
+};
+
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+};
+
+export default Header;
