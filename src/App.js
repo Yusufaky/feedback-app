f4c3c8807a63d76c73e973cabb96e46f59818289
Yusufaky <marcosta71999@gmail.com>
2022-11-28 17:05:31 +0000

    video 12

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React from "react";
 function App() {
-  const Titulo = "Titulo do blog";
-  const Corpo = "Corpo do blog";
-  const comentarios = [
-    { id: 1, text: "Comentario 1" },
-    { id: 2, text: "Comentario 2" },
-    { id: 3, text: "Comentario 3" },
-  ];
   return (
     <div className="container">
-      <h1>{Titulo}</h1>
-      <p>{Corpo}</p>
-      <div className="comentarios">
-        <h3>comentarios ({comentarios.length})</h3>
-        <ul>
-          {comentarios.map((comentario, index) => (
-            <li key={index}>{comentario.text}</li>
-          ))}
-        </ul>
-      </div>
+      <h1>My App</h1>
     </div>
   );
 }
