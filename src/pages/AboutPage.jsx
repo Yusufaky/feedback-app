75c9d72b2df4d6533979cba512696f419ed0e390
Yusufaky <marcosta71999@gmail.com>
2022-12-06 15:29:41 +0000

    27

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,13 @@
 import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
+
 function AboutPage() {
   return (
     <Card>
       <div className="about">
         <h1>This is About Page</h1>
         <p>
-          <a href="/">Back</a>
+          <Link to="/">Back</Link>
         </p>
       </div>
     </Card>
