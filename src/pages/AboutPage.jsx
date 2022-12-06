39913936286d0bac1216eaca5ab39cbc964d73d4
Yusufaky <marcosta71999@gmail.com>
2022-12-06 09:47:50 +0000

    26

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import Card from "../components/shared/Card";
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>This is About Page</h1>
+        <p>
+          <a href="/">Back</a>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
